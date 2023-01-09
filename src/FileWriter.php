15a7f144a0a719d56b9ddc706159f4b1ae20752d
molbal <molbal@outlook.com>
2023-01-09 10:11:14 +0100

    ♻️ Cleaned and indented DocBlock for FileWriter class, added cleanDocBlock method.

--- src/FileWriter.php
+++ src/FileWriter.php
@@ -15,11 +15,14 @@ class FileWriter
      * @return bool True if the docblock was written successfully, false otherwise
      */
     public function writeDocBlock(string $file, string $body, string $docblock): bool {
+
+        $docblock = $this->cleanDocBlock($docblock);
+
         $body = rtrim(explode('{', $body, 2)[0]);
         $docblock = $this->indentDocBlock(trim($docblock), $this->getLeadingWhitespace($body));
         $originalContents = file_get_contents($file);
-        $newContens = str_replace($body, $docblock . PHP_EOL . $body, $originalContents, $c);
-        file_put_contents($file, $newContens);
+        $newContents = str_replace($body, $docblock . PHP_EOL . $body, $originalContents, $c);
+        file_put_contents($file, $newContents);
         return  $c == 1;
     }
     /**
@@ -30,11 +33,12 @@ class FileWriter
      * @return string The leading whitespace from the string.
      */
 
-    private function getLeadingWhitespace(string $str) {
+    private function getLeadingWhitespace(string $str): string {
         $matches = [];
         $whitespace = preg_match('/^\s+/', $str, $matches) ? $matches[0] : '';
         return str_replace(PHP_EOL, '', $whitespace);
     }
+
     /**
      * Indents a DocBlock string with the given whitespace.
      *
@@ -43,13 +47,30 @@ class FileWriter
      *
      * @return string The indented DocBlock string.
      */
-
-
-    private function indentDocBlock(string $docs, string $whitespace) {
+    private function indentDocBlock(string $docs, string $whitespace): string
+    {
         $lines = explode(PHP_EOL, $docs);
         $modifiedLines = array_map(function($line) use ($whitespace) {
             return $whitespace . $line;
         }, $lines);
         return implode(PHP_EOL, $modifiedLines);
+    }
+
+
+    /**
+     * Clean a doc block from a given string.
+     * 
+     * @param string $str The string to clean.
+     * 
+     * @return string The cleaned doc block.
+     */
+    private function cleanDocBlock(string $str): string {
+        $startPos = strpos($str, '/**');
+        $endPos = strpos($str, '*/');
+        if ($startPos === false || $endPos === false) {
+            return '';
+        }
+        $startPos += 3;
+        return PHP_EOL.'/**'.PHP_EOL.trim(substr($str, $startPos, $endPos - $startPos)).'*/';
     }
 }
