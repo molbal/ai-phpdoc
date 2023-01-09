07350ff77a4c45482eb626d006cb53ee65066206
molbal <molbal@outlook.com>
2023-01-09 14:16:31 +0100

    🔧 Updated version number from 1.0.4 to 1.0.5 in bin/aiphpdocs and bin/aiphpdocs.php

--- bin/aiphpdocs.php
+++ bin/aiphpdocs.php
@@ -5,7 +5,7 @@ use Molbal\AiPhpdoc\ProcessDirectory;
 use Molbal\AiPhpdoc\ProcessFile;
 use Symfony\Component\Console\Application;
 
-$application = new Application('AI-PHPDocs by molbal', '1.0.4');
+$application = new Application('AI-PHPDocs by molbal', '1.0.5');
 
 $application->add(new ProcessFile());
 $application->add(new ProcessDirectory());
